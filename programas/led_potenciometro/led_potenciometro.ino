fb5e314e450623cb9d117541fcf944d7abb6e972
Alex-Costa08 <up202005259@edu.fe.up.pt>
2022-04-03 23:20:58 +0100

    updated led_potenciometro

--- programas/led_potenciometro/led_potenciometro.ino
+++ programas/led_potenciometro/led_potenciometro.ino
@@ -1,8 +1,11 @@
-int pinLed=8;
+int pinLed=3, pinPot=A3;
 void setup() {
   pinMode(pinLed, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(pinLed, HIGH);
+  int valor_medido;
+  valor_medido = analogRead(pinPot) ;
+  analogWrite(pinLed, valor_medido/4);
+  delay(100);
 }
